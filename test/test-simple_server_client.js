97cce56bc91225977da8c3e2f03e5263682eeb4e
Maxwell Krohn <themax@gmail.com>
2011-08-02 11:37:30 -0400

    one regtest now working, at least...

--- test/test-simple_server_client.js
+++ test/test-simple_server_client.js
@@ -1,22 +1,38 @@
 
 var msgpack_rpc = require('../lib/msgpack-rpc');
 
+
 module.exports = {
-    'simple server handler' : function(assert, beforeExit) {
+    'simple server handler' : function(test) {
 	var addition_called = false;
 	var response_received = false;
 	var notification_recieved = false;
+
+	var events = 2;
+
+	var trigger = function () {
+	    events--;
+	    if (events == 0) {
+		test.ok (addition_called);
+		test.ok (notification_received);
+		test.ok (response_received);
+		test.finish ();
+		server.close();
+	    }
+	}
+
+
 	var handler = {
 	    'add' : function(a, b, response) { 
-		console.log ("a=" + a + "; b=" + b);
-		console.log ("HELLLO! WTF");
 		addition_called = true;
 		response.result(a + b);
 	    },
 	    'temperature' : function(temp, response) {
-		assert.equal(102.1, temp);
-		assert.equal(undefined, response);
+		test.equal(102.1, temp);
+		test.equal(undefined, response);
 		notification_received = true;
+		trigger ();
+		
 	    }
 	};
 	
@@ -24,14 +40,12 @@ module.exports = {
 	server.setHandler(handler);
 	
 	server.listen(8030, function() {
-	    var client = msgpack_rpc.createClient(8030,function() {
-		console.log ("invoke add");
+	    var client = msgpack_rpc.createClient(null, 8030, function() {
 		client.invoke('add', 5, 7, function(err, response) {
-		    console.log ("resp: " + JSON.stringify (response));
 		    response_received = true;
-		    assert.equal(5 + 7, response);
-		    server.close();
+		    test.equal(5 + 7, response);
 		    client.stream.end();
+		    trigger ();
 		});
 		
 		client.notify('temperature', 102.1);
@@ -39,10 +53,5 @@ module.exports = {
 	    
 	});
 	
-	beforeExit(function() {
-	    assert.ok(addition_called);
-	    assert.ok(response_received);
-	    assert.ok(notification_received);
-	});
     },
 }
